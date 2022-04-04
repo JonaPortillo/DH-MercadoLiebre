3aca3ff7a9f9699b449bbffecfa7e8abcbdf78dc
JonaPortillo <portillo.jonathan.leonel@gmail.com>
2022-04-04 20:10:38 -0300

    Formularios - Desafío 1 - Creacion de formulario de registro, (ruta, css y html)

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+})
+
 
 app.listen(PORT, () => console.log(`
 Servidor escuchando en el puerto ${PORT}
